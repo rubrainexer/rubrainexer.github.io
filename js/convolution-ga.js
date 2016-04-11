7fad3f2eda5e1c997ae1cbd124f55d86ce819fd3
ptol <ptol@none>
2016-04-11 20:04:33 +0300

    fix

---
+++ js/convolution-ga.js
@@ -0,0 +1,17 @@
+﻿(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+var GA_LOCAL_STORAGE_KEY = 'ga:clientId';
+ga('create', 'UA-35739567-4', {
+	'storage': 'none',
+	'clientId': localStorage.getItem(GA_LOCAL_STORAGE_KEY)
+});
+ga('set', 'checkProtocolTask', null); 
+ga('set', 'checkStorageTask',null);
+ga(function(tracker) {
+	localStorage.setItem(GA_LOCAL_STORAGE_KEY, tracker.get('clientId'));
+});
+ga('send', 'pageview');
+
